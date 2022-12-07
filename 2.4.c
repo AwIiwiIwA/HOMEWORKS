22442933a27abce61593aed25b3ce479427cea35
Alehandro Krava <115242003+AwIiwiIwA@users.noreply.github.com>
2022-12-07 19:04:20 +0300

    Add files via upload

---
+++ 2.4.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define arraySize 10
+
+
+//Ќаписать программу, котора€ заполн€ет массив случайными значени€ми (с использованием функции rand из stdlib.h), 
+//потом преобразует его без использовани€ дополнительных массивов так, что в начале массива будут элементы, меньшие первого,
+//а в конце Ч большие либо равные первому. ѕрограмма должна работать за линейное врем€.
+
+
+void swap(int* firstElement, int* secondElement)
+{
+    int transitElement = *firstElement;
+    *firstElement = *secondElement;
+    *secondElement = transitElement;
+}
+
+
+void sort(int* array)
+{
+    int pivot = array[0];
+    int pivotIndex = 0;
+    int separatingIndexOfPivot = 0;
+
+    for (int i = 1; i < arraySize; ++i)
+    {
+        if (array[i] < pivot)
+        {
+            swap(&array[i], &array[pivotIndex]);
+            if (array[i] == pivot) separatingIndexOfPivot = i;
+            ++pivotIndex;
+        }
+    }
+
+    if (array[pivotIndex] != pivot)
+    {
+        swap(&array[separatingIndexOfPivot], &array[pivotIndex]);
+    }
+
+}
+
+void main(void)
+{
+
+    int array[arraySize] = { 0 };
+
+    srand(time(0));
+
+    for (int i = 0; i < arraySize; ++i)
+    {
+        array[i] = rand() % 32;
+    }
+    printf("Array before: ");
+    for (int i = 0; i < arraySize; ++i) printf("%d ", array[i]);
+    sort(array);
+    printf("\nArray after: ");
+    for (int i = 0; i < arraySize; ++i) printf("%d ", array[i]);
+
+}
