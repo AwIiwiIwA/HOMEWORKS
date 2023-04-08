22124418e85b9df56c0cd6efc854a87fc7c69f1d
Alehandro Krava <115242003+AwIiwiIwA@users.noreply.github.com>
2023-04-08 21:01:51 +0300

    HM-3 task-1

---
+++ FileName.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN_SIZE 10
+
+void insertionSort(int* arr, int left, int right) 
+{
+    for (int i = left + 1; i <= right; i++) 
+    {
+        int j = i;
+        int temp = arr[i];
+        while (j > left && arr[j - 1] > temp) 
+        {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = temp;
+    }
+}
+
+int partition(int* arr, int left, int right) 
+{
+    int pivot = arr[right];
+    int i = left - 1;
+    for (int j = left; j < right; j++) 
+    {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[right];
+    arr[right] = temp;
+    return i + 1;
+}
+
+void quicksort(int* arr, int left, int right) 
+{
+    if (right - left + 1 <= MIN_SIZE) 
+    {
+        insertionSort(arr, left, right);
+        return;
+    }
+    if (left < right) {
+        int pivot_index = partition(arr, left, right);
+        quicksort(arr, left, pivot_index - 1);
+        quicksort(arr, pivot_index + 1, right);
+    }
+}
+
+void qsortCustom(int* arr, int size)
+{
+    quicksort(arr, 0, size - 1);
+}
+
+int main() {
+    int arr[] = { 10, 5, 8, 1, 9, 3, 6, 7, 2, 4 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    qsortCustom(arr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
